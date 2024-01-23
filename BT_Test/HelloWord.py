3d924e5ba72be808c5eefa7350a8161a78e57f88
khanhhung0810 <lhkhungg@gmail.com>
2024-01-23 09:59:52 +0700

    new

---
+++ BT_Test/HelloWord.py
@@ -0,0 +1,6 @@
+if 5 > 2:
+    print("Five is greater than two!")
+name = "Khasnh Huwng"
+age = 22
+print(f"Hi there, my name is {name}, aged {age}")
+print("Hi there, my name is {}, aged {}".format(name, age))
